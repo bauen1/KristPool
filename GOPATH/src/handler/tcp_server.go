f71e3b952b31669a44df7f54a57a70ee3b8d12ee
Luca0208 <Luca@Lucas-MBP.fritz.box>
2016-01-24 00:50:59 +0100

    Added some code
    to convert a miner array in a json format that you can save into a file

--- GOPATH/src/handler/tcp_server.go
+++ GOPATH/src/handler/tcp_server.go
@@ -4,7 +4,12 @@ import(
 		"net"
 		"fmt"
 		"bufio"
+		//"encoding/json"
 	)
+type miner struct {
+	address string
+	solved int
+}
 func readFromConn(c net.Conn) string{
 	message,_ := bufio.NewReader(c).ReadString('\n')
 	return string(message)
@@ -28,3 +33,19 @@ func HandleTCP(){
 		go handleConn(conn)
 	}
 }
+
+/*
+Some code to convert miners into a json array(and maybe back)
+	miner1 := miner{"klucaund08",100}
+	miner2 := miner{"klucaunp08",190}
+	var miners [2]miner
+	miners[0] = miner1
+	miners[1] = miner2
+	var jsonArray [len(miners)][2]string
+	for i := 0; i < len(miners); i++ {
+		jsonArray[i][0] = miners[i].address
+		jsonArray[i][1] = string(miners[i].solved)
+	}
+	a,_ := json.Marshal(jsonArray);
+	fmt.Println(string(a))
+*/
