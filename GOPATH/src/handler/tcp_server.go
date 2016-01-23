b302c219a669bdd2243d2cc7e5fed54105732967
Luca0208 <Luca@Lucas-MBP.fritz.box>
2016-01-24 00:22:36 +0100

    Added some TCP read/write functions

--- GOPATH/src/handler/tcp_server.go
+++ GOPATH/src/handler/tcp_server.go
@@ -3,10 +3,19 @@ package handler
 import(
 		"net"
 		"fmt"
+		"bufio"
 	)
+func readFromConn(c net.Conn) string{
+	message,_ := bufio.NewReader(c).ReadString('\n')
+	return string(message)
+}
+func writeToConn(c net.Conn, s string){
+	c.Write([]byte(s + "\n"))
+}
 func handleConn(c net.Conn) {
-	fmt.Println(c.RemoteAddr())
-	c.Close()
+	for {
+		fmt.Println(readFromConn(c))
+	}
 }
 
 func HandleTCP(){
