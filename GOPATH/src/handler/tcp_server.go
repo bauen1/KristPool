cff7b1a7893ff43cc737145908b218e995bc71e1
Luca0208 <Luca@Lucas-MBP.fritz.box>
2016-01-24 09:50:48 +0100

    You can now save the miners and users to a file
    Also made some modifications to the miner struct.

--- GOPATH/src/handler/tcp_server.go
+++ GOPATH/src/handler/tcp_server.go
@@ -4,13 +4,40 @@ import(
 		"net"
 		"fmt"
 		"bufio"
-		//"encoding/json"
+		"io/ioutil"
+		"encoding/json"
 	)
+type user struct {
+	name string
+	password string //actually the password hash
+}
 type miner struct {
 	address string
 	prefix string
+	user string
 	shares int
-	balance int
+}
+var miners [1000]miner //I can expand this if needed
+var users [1000]user //I can expand this if needed
+func writeLists() { //FIXME: This exports it to an infinite long string.(See miners.json)
+	var minerArray [len(miners)][4]string
+	var userArray [len(users)][2]string
+	for i := 0; i < len(miners); i++ {
+		minerArray[i][0] = miners[i].address
+		minerArray[i][1] = miners[i].prefix
+		minerArray[i][2] = miners[i].user
+		minerArray[i][3] = string(miners[i].shares)
+	}
+	a,_ := json.Marshal(minerArray);
+	bytes := []byte(string(a))
+	err := ioutil.WriteFile("miners.json",bytes,0644)
+	if(err != nil) {
+		fmt.Println("Error while saving miners!")
+	}
+	for i := 0; i < len(users); i++ {
+		userArray[i][0] = users[i].name
+		userArray[i][1] = users[i].password
+	}
 }
 func readFromConn(c net.Conn) string{
 	message,_ := bufio.NewReader(c).ReadString('\n')
@@ -26,6 +53,9 @@ func handleConn(c net.Conn) {
 }
 
 func HandleTCP(){
+	miners[0] = miner{"klucaund08","7s","Luca",100}
+	users[0] = user{"Luca","someshastring"}
+	writeLists()
 	s,err:= net.Listen("tcp",":8392") //8392 is the port the miner will connect on (Don't ask)
 	if(err != nil){
 		fmt.Println(err)
@@ -35,14 +65,3 @@ func HandleTCP(){
 		go handleConn(conn)
 	}
 }
-
-/*
-Some code to convert miners into a json array(and maybe back)
-	var jsonArray [len(miners)][4]string
-	for i := 0; i < len(miners); i++ {
-		jsonArray[i][0] = miners[i].address
-		jsonArray[i][1] = string(miners[i].solved)
-	}
-	a,_ := json.Marshal(jsonArray);
-	fmt.Println(string(a))
-*/
